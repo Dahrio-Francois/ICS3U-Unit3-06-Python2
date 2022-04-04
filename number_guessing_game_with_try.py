29522f18be9433c74a1d88c01cf7a3dd9e05ab05
Dahrio-Francois <dahrio.francois@mths.ca>
2022-04-04 18:04:51 +0000

    made changes to guessing game

--- number_guessing_game_with_try.py
+++ number_guessing_game_with_try.py
@@ -13,21 +13,18 @@ some_variable = random.randint(0, 9)
 
 def main():
     # this program starts the game
-    integer_as_string = input(
-        "Enter an guess (between 0 & 9: "
-    )  # a number between 0 & 9
-    print("")
 
     # process & output
     try:
-        integer_as_number = int(integer_as_string)
-        print("You entered an integer correctly")
-        if integer_as_string == some_variable:
+        guess = int(input("Enter an guess (between 0 & 9: "))  # a number between 0 & 9
+
+        if guess == some_variable:
             print("\nCorrect! You guessed the right number!")
-        if integer_as_string != some_variable:
+
+        else:
             print("\nIncorrect! The correct number was {}.".format(some_variable))
     except Exception:
-        print("This was not an integer")
+        print("\nThis was not an integer")
     finally:
         print("\nTry again?")
 
